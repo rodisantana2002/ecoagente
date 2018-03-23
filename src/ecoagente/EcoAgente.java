1684fae43832f712141ebcb52ca808405cbef071
Rodolfo Santana <rodolfosmac@192.168.222.129>
2018-03-23 20:58:58 -0300

    inicio

--- src/ecoagente/EcoAgente.java
+++ src/ecoagente/EcoAgente.java
@@ -7,8 +7,8 @@ package ecoagente;
 
 import ecoagente.generic.model.Estado;
 import ecoagente.generic.model.Posicao;
-import ecoagente.mundoBlocos.model.Bloco;
 import ecoagente.mundoBlocos.control.MundoBlocos;
+import ecoagente.mundoBlocos.model.Bloco;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -20,7 +20,10 @@ public class EcoAgente {
 
     public static void main(String[] args) {
         Posicao objetivo, posInicial;
-        
+
+        //---------------------------------------------------------------------------------------------------
+        //  CONFIGURAÇÃO EXEMPLO 01
+        //---------------------------------------------------------------------------------------------------        
         posInicial = new Posicao(0, 0);
         objetivo = new Posicao(4, 1);
         Bloco bloco1 = new Bloco(1, "Bloco de Teste1", 'A', Estado.RS, posInicial, objetivo);       
@@ -41,7 +44,53 @@ public class EcoAgente {
         objetivo = new Posicao(0, 1);        
         Bloco bloco5 = new Bloco(5, "Bloco de Teste5", 'E', Estado.RS, posInicial, objetivo);
 
- 
+        
+        //---------------------------------------------------------------------------------------------------
+        //  CONFIGURAÇÃO EXEMPLO 02
+        //---------------------------------------------------------------------------------------------------        
+//        posInicial = new Posicao(0, 0);
+//        objetivo = new Posicao(4, 1);
+//        Bloco bloco1 = new Bloco(1, "Bloco de Teste1", 'A', Estado.RS, posInicial, objetivo);       
+//        
+//        posInicial = new Posicao(1, 0);       
+//        objetivo = new Posicao(3, 1);       
+//        Bloco bloco2 = new Bloco(2, "Bloco de Teste2", 'B', Estado.RS, posInicial, objetivo);
+//        
+//        posInicial = new Posicao(1,3);        
+//        objetivo = new Posicao(2, 1);        
+//        Bloco bloco3 = new Bloco(3, "Bloco de Teste3", 'C', Estado.RS, posInicial, objetivo);
+//        
+//        posInicial = new Posicao(0, 3);        
+//        objetivo = new Posicao(1, 1);        
+//        Bloco bloco4 = new Bloco(4, "Bloco de Teste4", 'D', Estado.RS, posInicial, objetivo);
+//        
+//        posInicial = new Posicao(0, 1);        
+//        objetivo = new Posicao(0, 1);        
+//        Bloco bloco5 = new Bloco(5, "Bloco de Teste5", 'E', Estado.RS, posInicial, objetivo);
+
+        //---------------------------------------------------------------------------------------------------
+        //  CONFIGURAÇÃO EXEMPLO 03
+        //---------------------------------------------------------------------------------------------------        
+//        posInicial = new Posicao(0, 0);
+//        objetivo = new Posicao(4, 1);
+//        Bloco bloco1 = new Bloco(1, "Bloco de Teste1", 'A', Estado.RS, posInicial, objetivo);       
+//        
+//        posInicial = new Posicao(1, 0);       
+//        objetivo = new Posicao(3, 1);       
+//        Bloco bloco2 = new Bloco(2, "Bloco de Teste2", 'B', Estado.RS, posInicial, objetivo);
+//        
+//        posInicial = new Posicao(1,3);        
+//        objetivo = new Posicao(2, 1);        
+//        Bloco bloco3 = new Bloco(3, "Bloco de Teste3", 'C', Estado.RS, posInicial, objetivo);
+//        
+//        posInicial = new Posicao(0, 3);        
+//        objetivo = new Posicao(1, 1);        
+//        Bloco bloco4 = new Bloco(4, "Bloco de Teste4", 'D', Estado.RS, posInicial, objetivo);
+//        
+//        posInicial = new Posicao(0, 1);        
+//        objetivo = new Posicao(0, 1);        
+//        Bloco bloco5 = new Bloco(5, "Bloco de Teste5", 'E', Estado.RS, posInicial, objetivo);
+        
         
         //adiciona configuração de blocos na lista que será passado ao processamento do Mundo dos Blocos
         List<Bloco> lstBlocos = new ArrayList<Bloco>();
